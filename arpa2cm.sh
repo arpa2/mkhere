63254998adc63cddddb29fd1303a1dc056b01fba
Rick van Rein <rick@openfortress.nl>
2019-10-02 20:30:38 +0000

    Introducing ARPA2CM into CMake setup.

---
+++ arpa2cm.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# kip build script
+
+. $(dirname "$0")/lib/stdlib
+
+# STABLE=0.0.0
+
+do_update () {
+	cd "$DIR_SRC"
+	empty_dir
+	git clone https://gitlab.com/arpa2/arpa2cm arpa2cm.git
+}
+
+do_touch () {
+	touch "$DIR_GIT"
+}
+
+do_dependencies () {
+	echo -n ''
+}
+
+do_build () {
+	if [ "$DIR_BUILD" -nt "$DIR_GIT" ]
+	then
+		echo 'Reusing build, as it postdates the source'
+	else
+		cd "$DIR_BUILD"
+		empty_dir
+		cmake -D CMAKE_INSTALL_PREFIX:PATH=/usr "$DIR_GIT"
+		make && \
+		empty_dir "$DIR_TREE" && \
+		make DESTDIR="$DIR_TREE" install
+	fi
+}
+
+do_list () {
+	cd "$DIR_TREE"
+	find . -type f,l | sed 's+^\./++'
+}
+
+do_variants () {
+	echo -n ''
+}
+
+do_flavours () {
+	echo -n ''
+}
+
+main_do_commands "$@"
+
