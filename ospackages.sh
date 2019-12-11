c3039bfbf7a8af1b0093e77becc3fa57e4a6bf2b
Rick van Rein <rick@openfortress.nl>
2019-12-11 15:27:51 +0000

    Support for OS package export  - The support is a mkhere script, /mkhere/ospackages.sh  - It responds to FLAVOUR_ospackages='bash gcc' and so on  - It can list and export archives, possibly also oslibs  - Need to install packages through     ".../ospackages.sh depend"  - Safe to run the needless command     ".../ospackages.sh have"  - Need to reference the rootfs through ".../ospackages.sh build"  - Common use is ".../ospackages.sh cpioprefix oslibcpio install"

---
+++ ospackages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# os package "build" script -- collects the files in OS packages
+#
+# A space-separated list of package names is in $FLAVOUR_ospackages
+# and will be used to determine the usual package aspects, such as
+# a list of dependencies and a list of file names.  The customary
+# packages and oslib packages can be made to retrieve the contents
+# and possibly install them.
+#
+# From: Rick van Rein <rick@openfortress.nl>
+
+
+. $(dirname "$0")/lib/stdlib
+
+do_touch () {
+	echo -n ''
+}
+
+do_update () {
+	echo -n ''
+}
+
+do_dependencies () {
+	for PKG in $FLAVOUR_ospackages
+	do
+		echo $PKG
+	done
+}
+
+do_build () {
+	find /* -maxdepth 0 | \
+		while read ROOTNAME
+		do
+			ln -s "$ROOTNAME" "$DIR_TREE$ROOTNAME"
+		done
+}
+
+do_list () {
+	# Specific for Debian packages -- may need a utility library
+	for PKG in $FLAVOUR_ospackages
+	do
+		dpkg -L $PKG | sed 's+^/++' | \
+			while read FILE
+			do
+				if [ ! -d "/$FILE" ]
+				then
+					echo "$FILE"
+				fi
+			done
+	done
+}
+
+do_variants () {
+	echo -n ''
+}
+
+do_flavours () {
+	# Not helpful to list all combinations of all packages
+	echo -n ''
+}
+
+main_do_commands "$@"
+
+
