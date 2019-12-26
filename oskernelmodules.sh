1265cc4329283c51f6b9e09d6767d767386fd4e6
Rick van Rein <rick@openfortress.nl>
2019-12-26 15:09:08 +0000

    Issue #6, support for kernels and kernel modules  - List kernels under VERSION/VARIANT constraints  - List kernel modules under FLAVOUR constraints  - Kernel modules are for the same kernels as listed  - Support list, but also cpio, cpiogz, cpioprefix, cpiogzprefix  - Note the prefixes may be used on top of a rootfs without  - TODO: Kernel module recursion

---
+++ oskernelmodules.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Find OS kernel modules, depending on what this means for the build OS.
+#
+# Setup modules in FLAVOUR_oskernelmodules.  Note that the variables
+# VERSION_oskernel and VARIANT_oskernel are meaningful too.
+#
+# From: Rick van Rein <rick@openfortress.nl>
+
+
+. $(dirname "$0")/lib/stdlib
+. $(dirname "$0")/lib/kernellib
+
+do_touch () {
+	echo -n ''
+}
+
+do_update () {
+	echo -n ''
+}
+
+do_dependencies () {
+	echo -n ''
+}
+
+do_osdependencies () {
+	echo -n ''
+}
+
+do_check () {
+	# Always out of date
+	return 1;
+}
+
+do_build () {
+	find /* -maxdepth 0 | \
+		while read ROOTNAME
+		do
+			if [ ! -h "$DIR_TREE$ROOTNAME" ]
+			then
+				ln -s "$ROOTNAME" "$DIR_TREE$ROOTNAME"
+			fi
+		done
+}
+
+do_list () {
+	kernel_modulefiles
+}
+
+do_variants () {
+	echo -n ''
+}
+
+do_flavours () {
+	echo -n ''
+}
+
+main_do_commands "$@"
