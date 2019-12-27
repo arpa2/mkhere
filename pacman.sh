1eea924960747767bc321c9b6b8e0fd7a04ecd21
Rick van Rein <rick@openfortress.nl>
2019-12-27 12:48:43 +0000

    Added /mkhere/pacman.sh -- the Arch Linux and MSYS2 package manager

---
+++ pacman.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# pacman build script (the package installer for Arch Linux and MSYS2)
+
+. $(dirname "$0")/lib/stdlib
+
+default_VERSION 5.2.1
+
+do_update () {
+	cd "$DIR_FETCH"
+	wget https://sources.archlinux.org/other/pacman/pacman-${VERSION}.tar.gz
+	cd "$DIR_SRC"
+	empty_dir "$DIR_SRC/pacman-${VERSION}"
+	tar -xzvf "$DIR_FETCH/pacman-${VERSION}.tar.gz"
+	do_touch
+}
+
+do_dependencies () {
+	echo -n ''
+}
+
+do_osdependencies () {
+	echo 'libarchive-dev'
+	echo 'libssl-dev'
+	echo 'pkg-config'
+}
+
+do_build2 () {
+	cd "$DIR_BUILD"
+	empty_dir
+	"$DIR_SRC/pacman-${VERSION}/configure" --prefix="/usr" && \
+	make && \
+	empty_dir "$DIR_TREE" && \
+	make DESTDIR="$DIR_TREE" install
+}
+
+do_list () {
+	cd "$DIR_TREE"
+	find . -type f,l | sed 's+^\./++'
+}
+
+do_variants () {
+	echo -n ''
+}
+
+do_flavours () {
+	echo -n ''
+}
+
+main_do_commands "$@"
+
