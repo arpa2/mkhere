1f71dd591fa87b358781d20fde2bf90332983465
Rick van Rein <rick@openfortress.nl>
2020-05-21 13:12:27 +0200

    Knot upgrade: add Python binding, version 2.9.4

--- knot.sh
+++ knot.sh
@@ -4,14 +4,15 @@
 
 . $(dirname "$0")/lib/stdlib
 
-default_VERSION 2.8.3
+default_VERSION 2.9.4
 
 do_update () {
 	cd "$DIR_FETCH"
 	wget https://secure.nic.cz/files/knot-dns/knot-${VERSION}.tar.xz
 	cd "$DIR_SRC"
-	empty_dir "$DIR_SRC/knot-${VERSION}"
-	tar -xJvf "$DIR_FETCH/knot-${VERSION}.tar.xz"
+	empty_dir "$DIR_SRC"
+	cd ..
+	tar --one-top-level="knot-${VERSION}" -xJvf "$DIR_FETCH/knot-${VERSION}.tar.xz"
 }
 
 do_dependencies () {
@@ -25,13 +26,18 @@ do_osdependencies () {
 	echo liblmdb-dev
 }
 
+# As long as Knot's Python code does not clone during ./configure
+# and fails to incorporate the libknot/control.py we need cpio below
 do2_build () {
 	cd "$DIR_BUILD"
 	empty_dir
-	"$DIR_SRC/knot-${VERSION}/configure" --prefix="/usr" && \
+	"$DIR_SRC/configure" --prefix="/usr" && \
 	make && \
 	empty_dir "$DIR_TREE" && \
-	make DESTDIR="$DIR_TREE" install
+	make DESTDIR="$DIR_TREE" install && \
+	( cd "$DIR_SRC" ; find python -name \*.py | cpio -o --quiet ) | cpio -i --quiet && \
+	cd "$DIR_BUILD/python" && \
+	python3 setup.py install --root="${DIR_TREE}" --prefix=/usr/local
 }
 
 do_test () {
